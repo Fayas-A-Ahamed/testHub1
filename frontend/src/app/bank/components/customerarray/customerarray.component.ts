11961e4dd9c76adc02379ff525a9c4198b09073f
username <wecp-dummy@gmail.com>
2024-08-04 16:59:02 +0000

    Day-18-19-20-21

--- frontend/src/app/bank/components/customerarray/customerarray.component.ts
+++ frontend/src/app/bank/components/customerarray/customerarray.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
-// import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-// import { of } from 'rxjs';
-import { CustomerTS } from '../../types/Customer';
+//import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+//import { of } from 'rxjs';
+import { CustomerTS } from '../../types/tstypes/Customerts';
 
 @Component({
   selector: 'app-customerarray',
-  standalone: true,
-  imports: [],
+  //  standalone: true,
+  //  imports: [],
   templateUrl: './customerarray.component.html',
   styleUrls: ['./customerarray.component.css']
 })
 export class CustomerarrayComponent {
-  customer:CustomerTS[] = [
-    new CustomerTS("John Doe", "john@gmail.com", "johnDoe", "johnDoe123", "Developer", "1"),
-    new CustomerTS("Jane Doe", "jane@gmail.com", "janeDoe", "janeDoe123", "Developer", "2"),
-    new CustomerTS("Jhonny Doe", "jhonny@gmail.com", "jhonnyDoe", "jhonnyDoe123", "Developer", "3")
-    ]
+  customers:CustomerTS[]=[
+    new CustomerTS('1','priya@gmail.com','Priya','priya','trainee'),
+    new CustomerTS('2','priya@gmail.com','Priya','priya','trainee'),
+    new CustomerTS('3','priya@gmail.com','Priya','priya','trainee'),
+    new CustomerTS('4','priya@gmail.com','Priya','priya','trainee')
+
+
+  ]
   
 }
